98dc0efa4537edfccb45399e531024f727618e01
Sushant Poman <sushantpoman94@gmail.com>
2024-02-21 20:06:03 +0530

    Added sleuth for MS tracing

--- src/main/java/com/example/orderservice/controller/OrderController.java
+++ src/main/java/com/example/orderservice/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.example.orderservice.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +20,9 @@ public class OrderController {
 
 	@Autowired
 	OrderRepository orderRepository;
+	
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
+
 	
 	@GetMapping("/greet")
 	public String greet(){
@@ -41,6 +46,7 @@ public class OrderController {
 	
 	@GetMapping("/orderByName/{username}")
 	public List<Orders> getOrdersByUsername(@PathVariable String username){
+		log.info("From OrderService");
 		return orderRepository.findByUsername(username);
 	}
 	
