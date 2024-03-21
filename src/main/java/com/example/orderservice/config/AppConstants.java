70aa7903ca3ca5b59bc9ec7f3ac368b22839f567
SushantPoman <sushantpoman94@gmail.com>
2024-03-21 20:42:35 +0530

    kafka - producer

---
+++ src/main/java/com/example/orderservice/config/AppConstants.java
@@ -0,0 +1,8 @@
+package com.example.orderservice.config;
+
+import io.micrometer.core.instrument.Statistic;
+
+public class AppConstants {
+
+	public final static String LOCATION_TOPIC_NAME ="location-update-topic";
+}
--- src/main/java/com/example/orderservice/controller/OrderController.java
+++ src/main/java/com/example/orderservice/controller/OrderController.java
@@ -1,9 +1,12 @@
 package com.example.orderservice.controller;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,12 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.orderservice.dao.Orders;
 import com.example.orderservice.repository.OrderRepository;
+import com.example.orderservice.service.KafkaService;
 
 @RestController
 public class OrderController {
 
 	@Autowired
 	OrderRepository orderRepository;
+	
+	@Autowired
+	KafkaService kafkaService;
 	
 	@GetMapping("/greet")
 	public String greet(){
@@ -42,6 +49,14 @@ public class OrderController {
 	@GetMapping("/orderByName/{username}")
 	public List<Orders> getOrdersByUsername(@PathVariable String username){
 		return orderRepository.findByUsername(username);
+	}
+	
+//	Kafka
+	@PostMapping("/update-loc")
+	public ResponseEntity<?> updateLocation(){
+		 kafkaService.updateLocation("(" +Math.random()*1000+" , "+Math.random()*1000+")");
+		 
+		 return new ResponseEntity<>(Map.of("message","Location updated"), HttpStatus.OK);
 	}
 	
 }
